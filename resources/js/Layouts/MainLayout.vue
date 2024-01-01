675e9e4005f76a6c278ff5b866997ffeb2e970d8
Laurin49 <welter@diwe.info>
2024-01-01 15:14:28 +0100

    dev10_authentication_and_security

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -8,13 +8,19 @@
         <div class="text-xl font-bold text-center text-indigo-600 dark:text-indigo-300">
           <Link :href="route('listing.index')">LaraZillow</Link>
         </div>
-        <div>
+        <div v-if="user" class="flex items-center gap-4">
+          <div class="text-sm text-gray-500">{{ user.name }}</div>
           <Link :href="route('listing.create')" class="btn-primary">+ New Listing</Link>
+          <Link :href="route('logout')" method="delete" as="button">Logout</Link>
+        </div>
+        <div v-else class="flex items-center gap-2">
+          <Link :href="route('user-account.create')">Register</Link>
+          <Link :href="route('login')">Sign-In</Link>
         </div>
       </nav>
     </div>
   </header>
-  <main class="container p-4 mx-auto">
+  <main class="container w-full p-4 mx-auto">
     <div v-if="flashSuccess" class="p-2 mb-4 border border-green-200 rounded-md shadow-sm dark:border-green-800 bg-green-50 dark:bg-green-900">
       {{ flashSuccess }}
     </div>
@@ -29,5 +35,8 @@ import { Link, usePage } from '@inertiajs/inertia-vue3'
 const page = usePage()
 const flashSuccess = computed(
   () => page.props.value.flash.success,
+)
+const user = computed(
+  () => page.props.value.user,
 )
 </script>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\IndexController;
 use App\Http\Controllers\ListingController;
+use App\Http\Controllers\UserAccountController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,13 +17,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::get('/', [IndexController::class, 'index']);
-Route::get('/hello', [IndexController::class, 'show']);
-Route::resource('listing', ListingController::class)->only([
-  'index',
-  'show',
-  'create',
-  'store',
-  'edit',
-  'update',
-  'destroy'
-]);
+Route::get('/hello', [IndexController::class, 'show'])->middleware('auth');
+
+// protecting routes for only authenticated users
+Route::resource('listing', ListingController::class)
+  ->only('create', 'store', 'edit', 'update', 'destroy')
+  ->middleware('auth');
+// routes for users the are not authenticated (no middleware behind)
+Route::resource('listing', ListingController::class)
+->only('show', 'index');
+
+Route::get('login', [AuthController::class, 'create'])->name('login');
+Route::post('login', [AuthController::class, 'store'])->name('login.store');
+Route::delete('logout', [AuthController::class, 'destroy'])->name('logout');
+
+Route::resource('user-account', UserAccountController::class)->only(['create', 'store']);
